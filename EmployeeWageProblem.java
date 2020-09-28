8fa21036d6802f488ee7cc5a921ca99dcd5d483b
7798209972 <patlesachin1@gmail.com>
2020-09-28 16:32:25 +0530

    Solving Using Switch Case Statement

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -14,6 +14,14 @@ public class EmployeeWageProblem {
 
 	public static final int PART_TIME_HOUR = 4;
 
+	/* Declaring Variables */
+
+	public int total_hours=0;
+	public int wage=0;
+	public int total_wage=0;
+	public int total_working_hour=0;
+	public String status;
+
 
 	public void get_employee_wage()
 	{
@@ -26,6 +34,24 @@ public class EmployeeWageProblem {
 		/* Using Random function for checking attendance */
 
 		int emp_attendance = (int) (Math.floor(Math.random()*10)%3);
+
+		/* Solving Using Switvh Case Statement */
+
+		switch(emp_attendance)
+		{
+			case IS_FULL_TIME :
+					status="Full time |";
+					total_hours=FULL_DAY_HOUR;
+					break;
+			case IS_PART_TIME:
+					status="Part time |";
+					total_hours=PART_TIME_HOUR;
+					break;
+			default :
+					status="Absent |";
+					total_hours=0;
+					break;
+		}
 		
 	}
 	public static void main(String[] args)
