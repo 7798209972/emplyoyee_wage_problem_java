241679988d28bdab42f2cedf8b7d3ff5c958aa14
7798209972 <patlesachin1@gmail.com>
2020-09-28 16:01:29 +0530

    Welcome Message

---
+++ EmployeeWageProblem.java
@@ -0,0 +1,20 @@
+public class EmployeeWageProblem {
+
+	public void get_employee_wage()
+	{
+		/* Welcome Message */
+
+		System.out.println("============================================");
+		System.out.println("Welcome to Employee Wage Computation Program");
+		System.out.println("============================================");
+		
+	}
+	public static void main(String[] args)
+	{
+		
+		EmployeeWageProblem obj=new EmployeeWageProblem();
+		obj.get_employee_wage();
+		
+		
+	}
+}
