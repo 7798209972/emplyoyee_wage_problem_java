496a70dc6e13de289b00b61310dcdc2fb63647bf
7798209972 <patlesachin1@gmail.com>
2020-09-28 16:20:13 +0530

    Calculating Daily Employee Wage

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -1,5 +1,16 @@
 public class EmployeeWageProblem {
 
+	/* Constants */
+
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+
+	/* Declaring Full Day Hour & Employee Wage Per Hour */
+	
+	public static final int FULL_DAY_HOUR = 8;
+	public static final int EMPLOYEE_RATE_PER_HOUR = 20;
+
+
 	public void get_employee_wage()
 	{
 		/* Welcome Message */
